979a7ca25314de926a3dd234a18189b1fd21ec5d
Ivan Kornev <chichinumber222@gmail.com>
2020-06-12 18:09:52 +0300

    Updates

---
+++ precache-manifest.aff2245f187cd5e0b8c9485e485018fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe09e87b83a12c9d20878a1b262667a",
+    "url": "/MyToDo/index.html"
+  },
+  {
+    "revision": "61e9c9c980fe541109dc",
+    "url": "/MyToDo/static/css/main.49bc889d.chunk.css"
+  },
+  {
+    "revision": "ce8262250e288787852c",
+    "url": "/MyToDo/static/js/2.06bc376d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyToDo/static/js/2.06bc376d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61e9c9c980fe541109dc",
+    "url": "/MyToDo/static/js/main.2054ec3c.chunk.js"
+  },
+  {
+    "revision": "e9a4019123ee4f697396",
+    "url": "/MyToDo/static/js/runtime-main.7b1ea13a.js"
+  }
+]);
