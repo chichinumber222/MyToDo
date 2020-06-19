214bd54ac680b925761f92f5a3fec5948e7ae581
Ivan Kornev <chichinumber222@gmail.com>
2020-06-19 21:35:42 +0300

    install eslint

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -4,10 +4,10 @@ import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
 class Task extends React.Component {
   state = {
-    previousCondition: '',
-    previousText: '',
+    previousCondition: "",
+    previousText: "",
     flag: true,
-    currentText: ''
+    currentText: "",
   };
 
   editInput = React.createRef();
@@ -16,31 +16,31 @@ class Task extends React.Component {
     this.setState({
       previousCondition: this.props.condition,
       previousText: this.props.text,
-      currentText: this.props.text
+      currentText: this.props.text,
     });
-    this.props.edit(this.props.id, {"condition": "editing"});
-    setTimeout(() => this.editInput.current.focus(), 0)
+    this.props.edit(this.props.id, { condition: "editing" });
+    setTimeout(() => this.editInput.current.focus(), 0);
   };
   editFnBlur = () => {
     if (this.state.flag) {
       const id = this.props.id;
       const prevCondition = this.state.previousCondition;
       const prevText = this.state.previousText;
-      this.props.edit(id, {'condition': prevCondition, 'text': prevText})
+      this.props.edit(id, { condition: prevCondition, text: prevText });
     }
-    this.setState({flag: true});
+    this.setState({ flag: true });
   };
   changeField = (e) => {
-    this.setState({currentText: e.target.value});
+    this.setState({ currentText: e.target.value });
   };
   submit = (e) => {
     e.preventDefault();
-    this.setState({flag: false});
+    this.setState({ flag: false });
     const id = this.props.id;
     const prevCondition = this.state.previousCondition;
     const currentText = this.state.currentText;
-    this.props.edit(id, {'condition': prevCondition, 'text': currentText});
-  }
+    this.props.edit(id, { condition: prevCondition, text: currentText });
+  };
 
   render() {
     const { condition, id, text, markComplete, del, time } = this.props;
@@ -59,11 +59,9 @@ class Task extends React.Component {
             <span className="description">{text}</span>
             <span className="created">{timeAgo} ago</span>
           </label>
+          <button className="icon icon-edit" onClick={this.editFn}></button>
           <button
-            className="icon icon-edit"
-            onClick={this.editFn}
-          ></button>
-          <button id="des"
+            id="des"
             className="icon icon-destroy"
             onClick={() => del(id)}
           ></button>
