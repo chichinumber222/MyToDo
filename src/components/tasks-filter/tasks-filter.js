a821ae30452b2ec6b0b20f90d8f22b554415c091
Ivan Kornev <chichinumber222@gmail.com>
2020-06-21 02:52:04 +0300

    eslint test and add rules

--- src/components/tasks-filter/tasks-filter.js
+++ src/components/tasks-filter/tasks-filter.js
@@ -1,14 +1,14 @@
-import React from "react";
-import "./tasks-filter.css";
+import React from 'react';
+import './tasks-filter.css';
 import PropTypes from 'prop-types';
 
 const TasksFilter = ({ onTab, tab }) => {
-  
   function createButton(n, fn, text) {
     return (
       <button
+        type="button"
         name={n}
-        className={tab === n ? "selected" : ""}
+        className={tab === n ? 'selected' : ''}
         onClick={(e) => fn(e.target.name)}
       >
         {text}
@@ -18,16 +18,16 @@ const TasksFilter = ({ onTab, tab }) => {
 
   return (
     <ul className="filters">
-      <li>{createButton("all", onTab, "All")}</li>
-      <li>{createButton("active", onTab, "Active")}</li>
-      <li>{createButton("completed", onTab, "Completed")}</li>
+      <li>{createButton('all', onTab, 'All')}</li>
+      <li>{createButton('active', onTab, 'Active')}</li>
+      <li>{createButton('completed', onTab, 'Completed')}</li>
     </ul>
   );
 };
 
 TasksFilter.propTypes = {
   onTab: PropTypes.func.isRequired,
-  tab: PropTypes.oneOf(["all", "active", "completed"]).isRequired,
+  tab: PropTypes.oneOf(['all', 'active', 'completed']).isRequired,
 };
 
 export default TasksFilter;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/app";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// const name = 'Ivan';
+// const a = (y) => y + 1;
+// a(2);
+// function man(n) {
+//   console.log(n);
+//   alert(n);
+//   n = 45;
+//   return n;
+// }
+
+// man(name);
